18f3efba2843644cfbf097247e5a9d08841e071d
AnnetaDe <anneta.liss@gmail.com>
2024-11-07 01:19:58 +0100

    pre_commit check

--- src/notes.py
+++ src/notes.py
@@ -8,7 +8,9 @@ from typing import List, Optional
 class Note:
     """Class representing a note."""
 
-    def __init__(self, title: str, body: str, tags: Optional[List[str]] = None, contacts: Optional[List[str]] = None) -> None:
+    def __init__(
+        self, title: str, body: str, tags: Optional[List[str]] = None, contacts: Optional[List[str]] = None
+    ) -> None:
         """Initialize the note.
         :param title: The title of the note.
         :param body: The body of the note.
@@ -19,12 +21,12 @@ class Note:
         self.title = title
         self.body = body
         self.creation_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        self._tags = tags if tags else []
-        self._contacts = contacts if contacts else []
+        self.tags = tags if tags else []
+        self.contacts = contacts if contacts else []
 
     def edit(self, new_body: str) -> None:
         """Edit the note by adding something to the body."""
-        self.body = self.body+' '+new_body
+        self.body = self.body + " " + new_body
 
     def replace(self, new_body: str) -> None:
         """Edit the note by replacing the body."""
@@ -32,33 +34,35 @@ class Note:
 
     def attach_to_contact(self, contact_name: str) -> None:
         """Attach the note to a contact."""
-        self._contacts.append(contact_name)
+        self.contacts.append(contact_name)
 
     def add_tag(self, tag: str) -> None:
         """Add a tag to the note."""
         if len(tag) < 0 and len(tag) > 20:
             raise ValueError("Tag must be between 1 and 20 characters")
-        self._tags.append(tag)
+        self.tags.append(tag)
 
     def remove_tag(self, tag: str) -> None:
         """Remove a tag from the note."""
-        self._tags.remove(tag)
+        self.tags.remove(tag)
 
     def __repr__(self):
-        return f"({self.title}, {self.creation_date}, {self.body}, {self._tags}, {self._contacts})"
+        return f"({self.title}, {self.creation_date}, {self.body}, {self.tags}, {self.contacts})"
 
 
 class NoteBook(UserDict):
     """Class representing a collection of notes."""
 
-    def add(self, title: str, body: str, tags: Optional[List[str]] = None, contacts: Optional[List[str]] = None) -> None:
+    def add(
+        self, title: str, body: str, tags: Optional[List[str]] = None, contacts: Optional[List[str]] = None
+    ) -> None:
         """Add a new note to the notebook."""
         self.data[title] = Note(title, body, tags, contacts)
 
     def delete(self, title: str) -> None:
         """Delete a note from the notebook by title."""
         if not title in self.data:
-            return KeyError(f"Note with title {title} not found")
+            raise KeyError(f"Note with title {title} not found")
         del self.data[title]
 
     def edit(self, title: str, new_body: str) -> None:
@@ -77,7 +81,11 @@ class NoteBook(UserDict):
         """Search for notes containing the query in their title or body."""
         if query == "":
             return list(self.data.values())
-        return [note for note in self.data.values() if query in note.title or query in note.body or query in note._tags or query in note._contacts]
+        return [
+            note
+            for note in self.data.values()
+            if query in note.title or query in note.body or query in note.tags or query in note.contacts
+        ]
 
     def add_tag(self, title: str, tag: str) -> None:
         """Add a tag to a note."""
