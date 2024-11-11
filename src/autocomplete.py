bd3829cb91b7c30690141d4c703491562bcfd10e
Volodymyr Pohoretskyi <v.pohoretskyi@gmail.com>
2024-11-11 18:44:40 +0100

    Addr 2nd level for autocomplete

--- src/autocomplete.py
+++ src/autocomplete.py
@@ -1,7 +1,6 @@
 """This module implements autocomplete functionality for the application."""
 
 from prompt_toolkit.completion import Completer, Completion
-from prompt_toolkit.document import Document
 
 from commands import Commands
 
@@ -11,20 +10,50 @@ class CommandCompleter(Completer):
     Class for handling autocompletion using the prompt_toolkit library.
     """
 
-    def get_completions(self, document: Document, complete_event) -> iter:
-        """Provide command suggestions based on user input.
+    def __init__(self, book, notes):
+        """Initialize the CommandCompleter class."""
+        self.book = book
+        self.notes = notes
 
-        Ignores completions after the first space and suggests commands starting with the input.
+    def get_completions(self, document, complete_event):
 
-        :param document: The current input text.
-        :param complete_event: Event passed by the prompt_toolkit (ignored here but must remain).
-        """
-        _ = complete_event  # Ignored because it's not used in this method, but cannot be removed
-        # because it's part of the required interface for the `Completer` class.
+        text = document.text_before_cursor.strip()
+        words = text.split()
+        if len(words) == 1 and not document.text_before_cursor.endswith(" "):
 
-        if len(document.text.split()) > 1:
-            return
+            for command in Commands.get_commands_list():
+                if document.text in command:
+                    yield Completion(command, start_position=-len(document.text))
 
-        for command in Commands.get_commands_list():
-            if document.text in command:
-                yield Completion(command, start_position=-len(document.text))
+        elif (len(words) == 1 and document.text_before_cursor.endswith(" ")) or (
+            len(words) == 2 and not document.text_before_cursor.endswith(" ")
+        ):
+
+            command = words[0]
+            if command in [
+                "add-address",
+                "add-phone",
+                "add-email",
+                "add-birthday",
+                "change",
+                "edit-email",
+                "edit-phone",
+            ]:
+
+                options = (
+                    [rec.name.value for rec in self.book.values() if words[1] in rec.name.value]
+                    if len(words) == 2
+                    else [rec.name.value for rec in self.book.values()]
+                )
+            elif command in ["add-tag", "remove-tag", "delete-note", "edit-note"]:
+                options = (
+                    [note for note in self.notes.data.keys() if words[1] in note]
+                    if len(words) == 2
+                    else self.notes.data.keys()
+                )
+            elif command == "cleanup":
+                options = ["all", "address-book", "notes"] if len(words) == 2 else []
+            else:
+                options = []
+            for opt in options:
+                yield Completion(opt, start_position=-(len(words[1]) if len(words) == 2 else 0))
